5316a1e303b5e4770d3d7b1e620c366b1c4359cd
janilsonfelipe <janilson_felipe@estudante.sc.senai.br>
2017-11-23 03:04:30 +0000

    Instalando heroku / tudo funcionando

--- app/models/aeronafe.rb
+++ app/models/aeronafe.rb
@@ -1,2 +1,12 @@
 class Aeronafe < ActiveRecord::Base
+
+    validates_presence_of :nome, on: :create, message: " não pode ficar em branco"
+    validates_presence_of :numero_de_tripulantes, on: :create, message: " não pode ficar em branco"
+    validates_presence_of :tamanho, on: :create, message: " não pode ficar em branco"
+    validates_presence_of :numero, on: :create, message: " não pode ficar em branco"
+    validates_presence_of :chassi, on: :create, message: " não pode ficar em branco"
+    validates_presence_of :quantidade_de_horas_de_voo, on: :create, message: " não pode ficar em branco"
+    validates_presence_of :quantidade_de_motor, on: :create, message: " não pode ficar em branco"
+    
+
 end
